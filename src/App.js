b0f98a52245de0e6de85120d0135a1bcab005e2c
yasgo <yasinburakkalkan@gmail.com>
2020-11-14 17:54:32 +0300

    App.js development

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import { useSelector } from 'react-redux'
 import PrivateRoute from './components/private-route'
 
