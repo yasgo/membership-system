4f2d31bae7814c1e8d56aee03f441b08ac2fe54e
yasgo <yasinburakkalkan@gmail.com>
2020-11-16 00:44:19 +0300

    Name input added for register form.

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,14 +1,28 @@
 import { auth } from '../../firebase';
-import NavigationProfile from '../navigation-profile'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Navigation = () => (
-    <nav>
-        {
-            auth.currentUser && (
-                <NavigationProfile />
-            )
-        }
-    </nav>
-)
+const Navigation = () => {
+
+    const onSignOut = () => {
+        auth.signOut()
+            .then(() => console.log('çıkış yapıldı!'))
+            .catch(error => console.log('Login Fail: ', error))
+    }
+
+    return (
+        <nav>
+            {
+                auth.currentUser && (
+                    <>
+                        <Link to='/profile'>Profile</Link>
+                        <FontAwesomeIcon onClick={onSignOut} icon={faSignOutAlt} />
+                    </>
+                )
+            }
+        </nav>
+    )
+}
 
 export default Navigation
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,15 +5,18 @@ import { auth } from '../firebase'
 
 const Register = () => {
 
-    const dispatch = useDispatch();
-    let [mail, setMail] = useState('');
-    let [password, setPassword] = useState('');
+    const dispatch = useDispatch()
+    let [mail, setMail] = useState('')
+    let [password, setPassword] = useState('')
+    let [name, setName] = useState('')
 
     const onSend = () => {
         dispatch({ type: LOADING_TOGGLE, isShow: true })
 
         auth.createUserWithEmailAndPassword(mail, password)
             .then(user => {
+                updateUserName()
+
                 dispatch({ type: LOADING_TOGGLE, isShow: false })
 
                 dispatch({
@@ -23,20 +26,29 @@ const Register = () => {
                     message: 'Başarıyla kayıt yaptınız!'
                 })
             })
-            .catch(error => {
-                dispatch({ type: LOADING_TOGGLE, isShow: false })
+            .catch(error => errorTrigger(error))
+    }
 
-                dispatch({
-                    type: ALERT_TOGGLE,
-                    isShow: true,
-                    isSuccess: false,
-                    message: error.message
-                })
-            })
+    const updateUserName = (user) => {
+        auth.currentUser.updateProfile({ displayName: name }).then(() => {
+            console.log('Kullanıcının ismi başarıyla değiştirildi!')
+        }).catch((error) => errorTrigger(error));
+    }
+
+    const errorTrigger = error => {
+        dispatch({ type: LOADING_TOGGLE, isShow: false })
+
+        dispatch({
+            type: ALERT_TOGGLE,
+            isShow: true,
+            isSuccess: false,
+            message: error.message
+        })
     }
 
     return (
         <>
+            <input type="text" placeholder="İsim" value={name} onChange={(e) => setName(e.target.value)} />
             <input type="mail" placeholder="Mail Adresi" value={mail} onChange={(e) => setMail(e.target.value)} />
             <input type="password" placeholder="Şifre" value={password} onChange={(e) => setPassword(e.target.value)} />
             <button onClick={onSend}>Üye Ol</button>
